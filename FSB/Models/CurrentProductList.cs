226d68e855d3d2ca3427a38741ca46046504bb11
mohammadrezabahrani <bahrani.mohammadreza@yahoo.com>
2023-10-16 13:38:59 +0330

    first commit

---
+++ FSB/Models/CurrentProductList.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FSB.Models
+{
+    public partial class CurrentProductList
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } = null!;
+    }
+}
